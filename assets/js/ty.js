591ef2ccb5bd83b6fa48a78ecb9bfa4c17b79bba
SamuelTessaro <samueldesouzatessaro@gmail.com>
2023-04-02 16:47:33 -0300

    alterações no site

---
+++ assets/js/ty.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Crie aplicativos sem linhas infinitas de códigos'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("forAnimateTy").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
